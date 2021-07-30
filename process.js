2ea9fe5ef6eb4861cafe6b9dea75e9924336f13e
Naomi De León <naomi.deleon@globant.com>
2021-07-30 16:43:54 -0500

    primer version del ejercicio 3 escritura de archivos'

--- process.js
+++ process.js
@@ -1,37 +1,91 @@
 const fs = require('fs');
+
 const schemaRules = require('./articleSchemaRules');
 const JSONvalidator = require('./validation');
 
-const enconding = 'utf8';
+const encoding = 'utf8';
 const fileLocation = './article.json';
 
-function validationHandler(article) {
+function getStringToSave(fileName, newContent) {
+    try {
+        const fileContent = fs.readFileSync(fileName, { encoding });
+        const jsonDB = JSON.parse(fileContent);
+        if (jsonDB.length === 0) {
+            return `[${JSON.stringify(newContent)}]`;
+        }
+        jsonDB.push(newContent);
+        return JSON.stringify(jsonDB);
+    } catch (err) {
+        return `[${JSON.stringify(newContent)}]`;
+    }
+}
+
+function writeOnFile(fileName, content) {
+    console.info(`Writing on ${fileName}`, content);
+    const line = getStringToSave(fileName, content);
+    try {
+        // flag a para permitir concatenar o crear el archivo en caso de no existir
+        fs.writeFileSync(fileName, line, { encoding });
+        console.info('file written');
+    } catch (err) {
+        console.err(err);
+    }
+}
+
+async function validationHandler(articleJSON) {
     console.info('Starting validation with manual handler');
-    const articleIsValid = JSONvalidator.validate(article, schemaRules.manual);
-    console.log(articleIsValid);
-}
-
-function yupValidationHandler(articleJSON) {
-    console.info('Starting validation with yup handler');
-    JSONvalidator.validateWithYup(articleJSON, schemaRules.yup).then(
-        (articleIsValid) => console.log(articleIsValid)
+    const articleIsValid = await JSONvalidator.validate(
+        articleJSON,
+        schemaRules.manual
     );
+    return articleIsValid;
 }
 
-function startJSONValidation(jsonValidatorHandler) {
-    fs.readFile(fileLocation, enconding, (err, jsonString) => {
-        if (err) {
-            console.log(`Error: ${err}`);
-            return;
-        }
-        try {
-            const article = JSON.parse(jsonString);
-            jsonValidatorHandler(article);
-        } catch (error) {
-            console.log(`Error: ${error}`);
-        }
-    });
+async function yupValidationHandler(articleJSON) {
+    console.info('Starting validation with yup handler');
+    const articleIsValid = await JSONvalidator.validateWithYup(
+        articleJSON,
+        schemaRules.yup
+    );
+    return articleIsValid;
 }
 
-startJSONValidation(yupValidationHandler);
-startJSONValidation(validationHandler);
+async function startJSONValidation(jsonValidatorHandler) {
+    try {
+        const fileContent = fs.readFileSync(fileLocation, encoding);
+        const articleJSON = JSON.parse(fileContent);
+        const articleIsValid = await jsonValidatorHandler(articleJSON);
+        const fileName = articleIsValid ? 'db.json' : 'invalid.json';
+        writeOnFile(fileName, articleJSON);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+}
+
+function startJSONValidationWithPromises(jsonValidatorHandler) {
+    return fs.promises
+        .readFile(fileLocation, encoding)
+        .then((fileContent) => JSON.parse(fileContent))
+        .then(async (articleJSON) => {
+            return {
+                articleJSON,
+                isValid: await jsonValidatorHandler(articleJSON),
+            };
+        })
+        .then((articleData) => {
+            const fileName = articleData.isValid ? 'db.json' : 'invalid.json';
+            writeOnFile(fileName, articleData.articleJSON);
+        })
+        .catch((err) => console.error(`Error: ${err}`));
+}
+
+async function startValidations() {
+    await startJSONValidation(validationHandler);
+    await startJSONValidation(yupValidationHandler);
+    console.info('First process finished', 'Launching promises');
+    startJSONValidationWithPromises(yupValidationHandler)
+        .then(() => startJSONValidationWithPromises(validationHandler))
+        .finally(() => console.log('Promises process finished'));
+}
+
+startValidations();
--- validation.js
+++ validation.js
@@ -4,55 +4,62 @@ async function validateWithYup(jsonObject, jsonRules) {
             .validate(jsonObject, { abortEarly: false })
             .catch((err) => err);
         if (result.errors) {
-            console.log(result.errors);
+            console.info('Validation failed');
+            console.error(result.errors);
             return false;
         }
-        console.log('Validation passed');
+        console.info('Validation passed');
         return true;
     } catch (err) {
-        console.log(err);
+        console.error(err);
         return false;
     }
 }
 
-function validate(jsonObject, jsonRules) {
-    const errors = [];
-    let jsonIsValid = true;
-    try {
-        Object.keys(jsonRules).forEach((jsonKey) => {
-            const { rule } = jsonRules[jsonKey];
-            const { dependencyRule } = jsonRules[jsonKey];
-            const value = jsonObject[jsonKey];
-            let validationResult;
-            if (rule) {
-                validationResult = rule(value);
-                if (validationResult !== null) {
-                    jsonIsValid = false;
-                    errors.push(validationResult);
+async function validate(jsonObject, jsonRules) {
+    const result = await new Promise((resolve) => {
+        try {
+            const errors = [];
+            let jsonIsValid = true;
+            Object.keys(jsonRules).forEach((jsonKey) => {
+                let validationResult;
+                const { rule } = jsonRules[jsonKey];
+                const { dependencyRule } = jsonRules[jsonKey];
+                const value = jsonObject[jsonKey];
+                if (rule) {
+                    validationResult = rule(value);
+                    if (validationResult !== null) {
+                        jsonIsValid = false;
+                        errors.push(validationResult);
+                    }
                 }
-            }
-            if (dependencyRule) {
-                const { dependencyField } = jsonRules[jsonKey];
-                validationResult = dependencyRule(
-                    value,
-                    jsonObject[dependencyField]
-                );
-                if (validationResult !== null) {
-                    jsonIsValid = false;
-                    errors.push(validationResult);
+                if (dependencyRule) {
+                    const { dependencyField } = jsonRules[jsonKey];
+                    validationResult = dependencyRule(
+                        value,
+                        jsonObject[dependencyField]
+                    );
+                    if (validationResult !== null) {
+                        jsonIsValid = false;
+                        errors.push(validationResult);
+                    }
                 }
+            });
+            if (!jsonIsValid) {
+                console.info('Validation failed');
+                console.error(errors);
+                resolve(false);
+                return;
             }
-        });
-        if (!jsonIsValid) {
-            console.log(errors);
-            return false;
+            console.info('Validation passed');
+            resolve(true);
+            return;
+        } catch (err) {
+            console.error(err);
+            resolve(false);
         }
-        console.log('Validation passed');
-        return true;
-    } catch (err) {
-        console.log(err);
-        return false;
-    }
+    });
+    return result;
 }
 
 const JSONvalidator = {
