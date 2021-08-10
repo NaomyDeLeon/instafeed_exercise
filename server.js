2f6d64d97b5eaa07b09d0ebfbbcdc1ea275bd1af
Naomi De León <naomi.deleon@globant.com>
2021-08-10 13:15:48 -0500

    ejercicio 6 Creación de un Servidor web con ExpressJS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const http = require('http');
 const fs = require('fs');
+const express = require('express');
 
+const app = express();
 const defaultPort = 8080;
-const articlesUrl = '/articles';
-const singleArticleUrl = /(articles\/[a-z0-9-]{36})/;
+const articlesPath = '/articles';
+const singleArticlePath = '/articles/:id';
 const dbPath = './db.json';
 let articlesDb;
+
 const loadArticlesDb = async () => {
     articlesDb = await fs.promises
         .readFile(dbPath)
@@ -14,40 +16,21 @@ const loadArticlesDb = async () => {
         .catch((err) => console.info(err));
 };
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    if (req.method === 'GET') {
-        if (req.url === articlesUrl) {
-            if (articlesDb !== undefined) {
-                res.statusCode = 200;
-                res.write(JSON.stringify(articlesDb));
-                res.end();
-            } else {
-                res.statusCode = 200;
-                res.write('unavailable retry in few minutes');
-                res.end();
-            }
-        } else if (singleArticleUrl.test(req.url)) {
-            if (articlesDb !== undefined) {
-                const articleId = req.url.split('/')[2];
-                console.log(articleId);
-                const articleJson = articlesDb.filter(
-                    (article) => article.id === articleId
-                );
-                res.statusCode = 200;
-                res.write(JSON.stringify(articleJson));
-                res.end();
-            } else {
-                res.statusCode = 200;
-                res.write('unavailable retry in few minutes');
-                res.end();
-            }
-        } else {
-            res.statusCode = 404;
-            res.write('invalid method');
-            res.end();
-        }
-    }
-});
-server.listen(defaultPort, () => console.log('servidor iniciado'));
+const dbIsAvailable = () => articlesDb !== undefined;
+
 loadArticlesDb();
+app.use(express.json());
+app.get(articlesPath, (req, res) => {
+    if (dbIsAvailable) res.send(articlesDb);
+    else res.send('unavailable retry in few minutes');
+});
+app.get(singleArticlePath, (req, res) => {
+    const articleId = req.params.id;
+    const articleJSON = articlesDb.filter(
+        (article) => article.id === articleId
+    );
+    if (dbIsAvailable) res.send(articleJSON);
+    else res.send('unavailable retry in few minutes');
+});
+
+app.listen(defaultPort, () => console.log('server iniciado'));
