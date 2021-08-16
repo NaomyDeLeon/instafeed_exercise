fac72bc227fb23f92eca4e9c5246ba2a5423e925
Naomi De León <naomi.deleon@globant.com>
2021-08-16 11:02:26 -0500

    ejercicio 16 A produccion

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { cpus } = require('os');
+const cluster = require('cluster');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -74,7 +76,27 @@ app.use(config.articlesPath, articleRouter);
 app.use(config.authorsPath, authorRouter);
 app.use(config.usersPath, userRouter);
 app.use(config.sessionsPath, sessionRouter);
-dbManager
-    .run()
-    .then(() => app.listen(config.port, () => console.log('server iniciado')))
-    .catch((err) => console.error(err));
+app.get(config.crashPath, () => process.exit(0));
+
+try {
+    const numCPUs = cpus().length;
+    if (cluster.isMaster) {
+        console.log(`Primary ${process.pid} is running`);
+        for (let i = 0; i < numCPUs; i += 1) {
+            cluster.fork();
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(
+                `worker ${worker.process.pid} died ${code} - ${signal}`
+            );
+        });
+    } else {
+        dbManager
+            .run()
+            .then(() => app.listen(config.port, () => console.log('iniciado')))
+            .catch((err) => console.error(err));
+        console.log(`Worker ${process.pid} started`);
+    }
+} catch (err) {
+    console.error(err);
+}
